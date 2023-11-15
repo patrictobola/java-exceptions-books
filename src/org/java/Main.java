5866d986cd425ed7e3b2defb4ca9d312cf3e7501
patrictobola <tobola92@gmail.com>
2023-11-15 15:47:09 +0100

    added writer and tested

--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -1,9 +1,13 @@
 package org.java;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+	static final File mioFile = new File("D:\\test.txt");
+			
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
@@ -38,11 +42,36 @@ public class Main {
 			
 		}
 		in.close();
+		
+		
+		
+		FileWriter myWriter = null;
+		try {
+		    myWriter = new FileWriter(mioFile);
+
+		    for (Book book : library) {
+		        myWriter.write(book.toString() + "\n");
+		    }
+		    System.out.println("\nDati dei libri scritti su 'test.txt'");
+		} catch (IOException e) {
+		    System.out.println("Errore durante la scrittura su file: " + e.getMessage());
+		} finally {
+		    if (myWriter != null) {
+		        try {
+		            myWriter.close();
+		        } catch (IOException e) {
+		            // Gestione dell'eccezione durante la chiusura del file
+		            System.out.println("Errore durante la chiusura del file: " + e.getMessage());
+		        }
+		    }
+		}
+		
+		
 //		System.out.println(Arrays.asList(library));
-		System.out.println("Libri:" + '\n'
-				+ "---------------------------------------------");
-        for (Book book : library) {
-            System.out.println(book.toString());
-        }
+//		System.out.println("Libri:" + '\n'
+//				+ "---------------------------------------------");
+//        for (Book book : library) {
+//            System.out.println(book.toString());
+//        }
 	}
 }
