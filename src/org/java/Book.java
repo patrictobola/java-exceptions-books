33e158362f0b875170b9db63471c23502a2d8948
patrictobola <tobola92@gmail.com>
2023-11-15 14:14:09 +0100

    first commit

---
+++ src/org/java/Book.java
@@ -0,0 +1,44 @@
+package org.java;
+
+public class Book {
+	private String title;
+	private int pages;
+	private String author;
+	private String publisher;
+	
+	public Book(String title, int pages, String author, String publisher) {
+		
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPages() {
+		return pages;
+	}
+
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+}
